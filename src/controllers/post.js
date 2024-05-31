bf58c2c00f1cae7f36deb28ee530fb1775208e5f
TranVanHuu732002 <Huukoi732002@gmail.com>
2024-05-31 08:58:08 +0700

    Api Create New Post

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -13,9 +13,12 @@ export const getPosts = async (req, res) => {
 };
 // Phan trang,Theo gia,dien tich
 export const getPostsLimit = async (req, res) => {
-  const { page ,priceNumber , areaNumber,...query } = req.query;
+  const { page, priceNumber, areaNumber, ...query } = req.query;
   try {
-    const response = await postService.getPostsLimitService(page,query,{priceNumber , areaNumber});
+    const response = await postService.getPostsLimitService(page, query, {
+      priceNumber,
+      areaNumber,
+    });
     return res.status(200).json(response);
   } catch (error) {
     return res.status(500).json({
@@ -28,6 +31,42 @@ export const getPostsLimit = async (req, res) => {
 export const getNewPosts = async (req, res) => {
   try {
     const response = await postService.getNewPostsService();
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Failed at post controller " + error,
+    });
+  }
+};
+
+// Tao bai dang moi
+export const createNewPost = async (req, res) => {
+  try {
+    const {
+      categoryCode,
+      title,
+      priceNumber,
+      areaNumber,
+      label,
+      ...payload
+    } = req.body;
+    const { id } = req.user;
+    console.log(id)
+    if (
+      !categoryCode ||
+      !id||
+      !title ||
+      !priceNumber ||
+      !areaNumber ||
+      !label
+    )
+      return res.status(400).json({
+        err: 1,
+        msg: "Missing inputs",
+      });
+
+    const response = await postService.createNewPostService(req.body,id);
     return res.status(200).json(response);
   } catch (error) {
     return res.status(500).json({
--- src/services/post.js
+++ src/services/post.js
@@ -1,5 +1,8 @@
 import { Op } from "sequelize";
 import db from "../models";
+import { v4 as uuid } from "uuid";
+import moment from "moment";
+import generateCode from "../utils/generateCode";
 
 export const getPostsService = () =>
   new Promise(async (resolve, reject) => {
@@ -36,19 +39,24 @@ export const getPostsService = () =>
     }
   });
 // Phan trang,theo gia, dien tiich
-export const getPostsLimitService = (page, query ,{priceNumber , areaNumber}) =>
+export const getPostsLimitService = (
+  page,
+  query,
+  { priceNumber, areaNumber }
+) =>
   new Promise(async (resolve, reject) => {
     try {
       let offset = !page || +page <= 1 ? 0 : +page - 1;
-      const queries = { ...query}
-      if(priceNumber) queries.priceNumber = { [Op.between] : priceNumber}
-      if(areaNumber) queries.areaNumber = { [Op.between] : areaNumber}
+      const queries = { ...query };
+      if (priceNumber) queries.priceNumber = { [Op.between]: priceNumber };
+      if (areaNumber) queries.areaNumber = { [Op.between]: areaNumber };
 
       const response = await db.Post.findAndCountAll({
         where: queries,
         raw: true,
         nest: true,
         offset: offset * +process.env.LIMIT,
+        order: [["createdAt", "DESC"]],
         limit: +process.env.LIMIT,
         include: [
           {
@@ -106,6 +114,94 @@ export const getNewPostsService = () =>
         err: response ? 0 : 1,
         msg: response ? "OK" : "Failed to get posts.",
         response,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+
+// Tao bai dang moi
+export const createNewPostService = (body, userId) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const attributesId = uuid();
+      const imagesId = uuid();
+      const overviewId = uuid();
+      const labelCode = generateCode(body.label);
+      const hashtag = `${Math.floor(Math.random() * Math.pow(10, 6))}`;
+      const currentDate = new Date();
+      await db.Post.create({
+        id: uuid(),
+        title: body.title,
+        labelCode,
+        address: body.address || null,
+        attributesId,
+        categoryCode: body.categoryCode,
+        description: JSON.stringify(body.description) || null,
+        userId,
+        overviewId,
+        imagesId,
+        areaCode: body.areaCode || null,
+        priceCode: body.priceCode || null,
+        provinceCode: body?.province?.includes("Thành phố")
+          ? generateCode(body?.province?.replace("Thành phố ", ""))
+          : generateCode(body?.province?.replace("Tỉnh ", "")) || null,
+        priceNumber: body.priceNumber,
+        areaNumber: body.areaNumber,
+      });
+      await db.Attribute.create({
+        id: attributesId,
+        price:
+          +body.priceNumber < 1
+            ? `${+body.priceNumber * 1000000} đồng/tháng`
+            : `${+body.priceNumber} triệu/tháng`,
+        acreage: `${body.areaNumber} m2`,
+        published: moment(new Date()).format("DD/MM/YYYY"),
+        hashtag,
+      });
+      await db.Image.create({
+        id: imagesId,
+        image: JSON.stringify(body.images),
+      });
+      await db.Overview.create({
+        id: overviewId,
+        code: `#${hashtag}`,
+        area: body?.label,
+        type: body?.category,
+        target: body?.target,
+        bouns: "Tin thường",
+        created: currentDate,
+        expire: new Date(currentDate.getTime() + 3 * 24 * 60 * 60 * 1000),
+      });
+
+      await db.Province.findOrCreate({
+        where: {
+          [Op.or]: [
+            { value: body?.province.replace("Thành phố ", "") },
+            { value: body?.province.replace("Tỉnh ", "") },
+          ],
+        },
+        defaults: {
+          code: body?.province?.includes("Thành phố")
+            ? generateCode(body?.province?.replace("Thành phố ", ""))
+            : generateCode(body?.province?.replace("Tỉnh ", "")),
+          value: body?.province?.includes("Thành phố")
+            ? body?.province?.replace("Thành phố ", "")
+            : body?.province?.replace("Tỉnh ", ""),
+        },
+      });
+      await db.Label.findOrCreate({
+        where: {
+          code: labelCode,
+        },
+        defaults: {
+          code: labelCode,
+          value: body.label,
+        },
+      });
+      resolve({
+        err: 0,
+        msg: "OK",
       });
     } catch (error) {
       reject(error);

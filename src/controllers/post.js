afac4430b3a35fd3bed9bfe3ea3a4200f7dd40d1
TranVanHuu732002 <Huukoi732002@gmail.com>
2024-06-01 09:18:38 +0700

    Api get post follow user

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -52,7 +52,6 @@ export const createNewPost = async (req, res) => {
       ...payload
     } = req.body;
     const { id } = req.user;
-    console.log(id)
     if (
       !categoryCode ||
       !id||
@@ -67,6 +66,24 @@ export const createNewPost = async (req, res) => {
       });
 
     const response = await postService.createNewPostService(req.body,id);
+    return res.status(200).json(response);
+  } catch (error) {
+    return res.status(500).json({
+      err: -1,
+      msg: "Failed at post controller " + error,
+    });
+  }
+};
+// Ben admin
+export const getPostsLimitAdmin = async (req, res) => {
+  const { page,  ...query } = req.query;
+  const {id} =req.user
+  try {
+    if(!id) return res.status(400).json({
+      err: 1,
+      msg: 'Missing inputs'
+    })
+    const response = await postService.getPostsLimitAdminService(page,id, query);
     return res.status(200).json(response);
   } catch (error) {
     return res.status(500).json({
--- src/services/post.js
+++ src/services/post.js
@@ -208,3 +208,50 @@ export const createNewPostService = (body, userId) =>
       reject(error);
     }
   });
+
+// ben admin
+export const getPostsLimitAdminService = (page,id, query) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      let offset = !page || +page <= 1 ? 0 : +page - 1;
+      const queries = { ...query ,userId:id};
+
+      const response = await db.Post.findAndCountAll({
+        where: queries,
+        raw: true,
+        nest: true,
+        offset: offset * +process.env.LIMIT,
+        order: [["createdAt", "DESC"]],
+        limit: +process.env.LIMIT,
+        include: [
+          {
+            model: db.Image,
+            as: "images",
+            attributes: ["image"],
+          },
+          {
+            model: db.Attribute,
+            as: "attributes",
+            attributes: ["price", "acreage", "published", "hashtag"],
+          },
+          {
+            model: db.User,
+            as: "user",
+            attributes: ["name", "zalo", "phone"],
+          },
+          {
+            model: db.Overview,
+            as: "overviews",
+          },
+        ],
+        //attributes: ["id", "title", "star", "address", "description"],
+      });
+      resolve({
+        err: response ? 0 : 1,
+        msg: response ? "OK" : "Failed to get posts.",
+        response,
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
